596ea7324ed8571f8a7bfe8ec8ed9c8bb23f9139
Ayelén Silva <ayelen.sl@gmail.com>
2026-04-20 12:48:49 -0400

    Inicializa proyecto

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    <nav>
+
+    </nav>
+  </header>
+  <main>
+    <h1>Portafolio profesional</h1>
+  </main>
+</template>
+
+<style scoped></style>
